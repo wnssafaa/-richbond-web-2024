a2073115aeb7660300a0a268dea0216725913642
SafaaElOuanass <wannassafaa8@gmail.com>
2025-10-10 15:08:30 +0100

    changment de api de map

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  thunderforestApiKey: '149390220ffa418f8da30dc0ab42d849', // ⚠️ Remplacez par votre vraie clé API
+  thunderforestApiKey: '149390220ffa418f8da30dc0ab42d849', 
 };
